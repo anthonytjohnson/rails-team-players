2610632c6d505d8ce367c52abbb96b177cb0343b
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-14 15:17:30 +0900

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'teams#index'
+
+  resources :teams, only: [:index, :show, :new, :create, :destroy] do
+    resources :team_players, only: [:new, :create]
+  end
+  resources :players, only: [:show] do
+    resources :player_skills, only: [:new, :create]
+  end
 end
