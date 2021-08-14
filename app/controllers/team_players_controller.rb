4302bca8de5d19060d9cd8b678fdfe4a25adf831
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-14 20:48:23 +0900

    able to add players

--- app/controllers/team_players_controller.rb
+++ app/controllers/team_players_controller.rb
@@ -2,13 +2,19 @@ class TeamPlayersController < ApplicationController
   before_action :set_team, only: [:new, :create]
 
   def new
-    @team_player = Team_player.new
+    @team_player = TeamPlayer.new
     @players = Player.where.not(id: @team.players).order(name: :asc)
+    @centers = Player.where(position: 'center')
+    @powerforwards = Player.where(position: 'power forward')
+    @smallforwards = Player.where(position: 'small forward')
+    @shootingguards = Player.where(position: 'shooting guard')
+    @pointguards = Player.where(position: 'point guard')
+    @sixthmen = Player.where(position: '6th man')
   end
 
   def create
-    @team_player = Team_player.new(team_player_params)
-    @team_player.list = @list
+    @team_player = TeamPlayer.new(team_player_params)
+    @team_player.team = @team
     # @bookmark.movie = @movie
     if @team_player.save
       redirect_to team_path(@team)
--- app/models/team_player.rb
+++ app/models/team_player.rb
@@ -3,4 +3,5 @@ class TeamPlayer < ApplicationRecord
   belongs_to :player
 
   validates :team, uniqueness: { scope: :player }
+  accepts_nested_attributes_for :player, reject_if: proc { |att| att['name'].blank? }
 end
