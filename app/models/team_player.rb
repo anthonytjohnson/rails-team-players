dcd7db520d7fc065fd50a8563794d17dc647aa54
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-15 10:10:19 +0900

    fixed player add form

--- app/models/team_player.rb
+++ app/models/team_player.rb
@@ -3,5 +3,4 @@ class TeamPlayer < ApplicationRecord
   belongs_to :player
 
   validates :team, uniqueness: { scope: :player }
-  accepts_nested_attributes_for :player, reject_if: proc { |att| att['name'].blank? }
 end
